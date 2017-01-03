52cffb2f566ab9cd431606e3e073c6275ce84eb7
David Kowis <david_kowis@homedepot.com>
2017-01-02 21:08:58 -0600

    Make it part of the darwin operating system section, not the default

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,14 +18,15 @@ class gcc {
         ensure  => '5.1.0',
         require => Homebrew::Tap['homebrew/versions']
       }
+
+      package { ['boxen/brews/apple-gcc42', 'boxen/brews/gcc48']:
+        ensure => 'absent'
+      }
     }
 
     default: {
       package { 'gcc': }
 
-      package { ['boxen/brews/apple-gcc42', 'boxen/brews/gcc48']:
-        ensure => 'absent'
-      }
     }
   }
 
